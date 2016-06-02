36a064043623c47cdfa613599812fdcb93685245
spsamjesus <spsamjesus@gmail.com>
2016-06-02 07:29:50 +0530

    My app fils

---
+++ app/src/androidTest/java/com/example/android/session4assignment1/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.android.session4assignment1;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Sam on 5/30/2016.
+ */
+public class MainActivityTest extends TestCase {
+
+}
